2c76e2a0cae05598e46238f8fc3443a0b8fcf1da
AlexanderPatsukov <85254220+AlexanderPatsukov@users.noreply.github.com>
2022-09-06 01:34:09 +0300

    AraAra

---
+++ index.js
@@ -0,0 +1,30 @@
+import pMap from 'p-map';
+import got from 'got';
+import fs from 'fs'
+
+let target_site = 'https://gelbooru.com/index.php?page=dapi&s=tag&q=index&json=1&orderby=count&limit=1000&pid='
+
+const iteration_count = await got(target_site.replace('limit=1000', 'limit=1') + '0')
+.then((Response)=>{
+	return Math.ceil(JSON.parse(Response.body)['@attributes'].count / 1000)
+});
+
+const sites = Array.from(Array(iteration_count).keys(), (e)=>{return target_site + e})
+
+const mapper = async site => {
+	let requestUrl = await got(site)
+	.then((Response)=>{
+		return JSON.parse(Response.body).tag
+	});
+	console.clear()
+	console.log(`Progress ${site.split('pid=')[1]}/${iteration_count}`)
+	return requestUrl;
+};
+
+const result = await pMap(sites, mapper, {concurrency: 10});
+
+const unite_data = result.reduce(
+	(arr1, arr2) => arr1.concat(arr2),
+	[])
+
+fs.writeFileSync('db.json', JSON.stringify(unite_data))
